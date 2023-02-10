18ba49450e719278a1291ef519859696b5463d93
andrew <andrew.vorobiev.work@gmail.com>
2023-02-10 17:30:39 +0300

    Создаю простой макет приложения, в котором буду учиться использовать Корутины.
    Также добавил простой код, на котором буду учитья писать корутины.
    Поэтому он будет базовым

--- app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.example.kotlincoroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +20,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.buttonLoad.setOnClickListener {
-            loadData()
+            // Если ты хочешь, чтобы после того как Activity прекращала свою работу,
+            // то все запросы в потоке отменялись, то есть чтобы у них был жизненный цикл,
+            // для этого надо использовать метод lifecycleScope.launch
+            lifecycleScope.launch{
+                loadData()
+            }
         }
     }
 
-    private fun loadData() {
+
+    // Чтобы не возникала ошибка в основном методе, необходимо также добавить suspend
+    private suspend fun loadData() {
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
         val city = loadCity()
@@ -32,18 +42,22 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.isEnabled = true
     }
 
-    private fun loadCity(): String {
-        Thread.sleep(5000)
+
+    // Чтобы этот метод можно было приостановить и не заблокировать основной поток,
+    // нужно добавить слово suspend
+    private suspend fun loadCity(): String {
+        // А Thread.sleep заменяем -> на delay(Число в формате Int - 1000 == Одна секунда)
+        delay(5000)
         return "Moscow"
     }
 
-    private fun loadTemperature(city: String): Int {
+    private suspend fun loadTemperature(city: String): Int {
         Toast.makeText(
             this,
             getString(R.string.loading_temperature_toast, city),
             Toast.LENGTH_SHORT
         ).show()
-        Thread.sleep(5000)
+        delay(5000)
         return 17
     }
 }
