c8ed96a2f98f561e0db9f4076229051044fc10fc
andrew <andrew.vorobiev.work@gmail.com>
2023-02-16 11:46:21 +0300

    Создаю простой макет приложения, в котором буду учиться использовать Корутины.
    Также добавил простой код, на котором буду учитья писать корутины.
    Поэтому он будет базовым

--- app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kotlincoroutines
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -23,23 +26,25 @@ class MainActivity : AppCompatActivity() {
             // Если ты хочешь, чтобы после того как Activity прекращала свою работу,
             // то все запросы в потоке отменялись, то есть чтобы у них был жизненный цикл,
             // для этого надо использовать метод lifecycleScope.launch
-            lifecycleScope.launch{
-                loadData()
+            binding.progress.isVisible = true
+            binding.buttonLoad.isEnabled = false
+            val jobCity = lifecycleScope.launch {
+                val city = loadCity()
+                binding.tvLocation.text = city
             }
+            val jobTemp = lifecycleScope.launch {
+                val temp = loadTemperature()
+                binding.tvTemperature.text = temp.toString()
+            }
+            lifecycleScope.launch {
+                // метод join - остановит корутину до тех пор, пока эта работа не будет выполнена
+                jobCity.join()
+                jobTemp.join()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+//            loadWithoutCoroutine()
         }
-    }
-
-
-    // Чтобы не возникала ошибка в основном методе, необходимо также добавить suspend
-    private suspend fun loadData() {
-        binding.progress.isVisible = true
-        binding.buttonLoad.isEnabled = false
-        val city = loadCity()
-        binding.tvLocation.text = city
-        val temp = loadTemperature(city)
-        binding.tvTemperature.text = temp.toString()
-        binding.progress.isVisible = false
-        binding.buttonLoad.isEnabled = true
     }
 
 
@@ -51,13 +56,72 @@ class MainActivity : AppCompatActivity() {
         return "Moscow"
     }
 
-    private suspend fun loadTemperature(city: String): Int {
+    private suspend fun loadTemperature(): Int {
+        delay(5000)
+        return 17
+    }
+
+
+
+    private fun loadWithoutCoroutine(step: Int = 0, obj: Any? = null) {
+        when (step) {
+            0 -> {
+                Log.d("MainActivity", "Load started: $this")
+                binding.progress.isVisible = true
+                binding.buttonLoad.isEnabled = false
+                loadCityWithoutCoroutine {
+                    loadWithoutCoroutine(1, it)
+                }
+            }
+            1 -> {
+                val city = obj as String
+                binding.tvLocation.text = city
+                loadTemperatureWithoutCoroutine(city) {
+                    loadWithoutCoroutine(2, it)
+                }
+            }
+            2 -> {
+                val temp = obj as Int
+                binding.tvTemperature.text = temp.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+                Log.d("MainActivity", "Load finished: $this")
+            }
+        }
+    }
+
+
+    private fun loadCityWithoutCoroutine(callback: (String) -> Unit) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback.invoke("Moscow")
+        }, 5000)
+    }
+
+
+    // Чтобы не возникала ошибка в основном методе, необходимо также добавить suspend
+    private suspend fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        val city = loadCity()
+        binding.tvLocation.text = city
+        val temp = loadTemperature()
+        binding.tvTemperature.text = temp.toString()
+        binding.progress.isVisible = false
+        binding.buttonLoad.isEnabled = true
+    }
+
+
+    private fun loadTemperatureWithoutCoroutine(city: String, callback: (Int) -> Unit) {
         Toast.makeText(
             this,
             getString(R.string.loading_temperature_toast, city),
             Toast.LENGTH_SHORT
         ).show()
-        delay(5000)
-        return 17
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback.invoke(17)
+        }, 5000)
     }
+
+
 }
